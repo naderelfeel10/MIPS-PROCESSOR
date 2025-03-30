d7b95974f44ae10e6be8a048fbf731049d05bab7
naderelfeel10 <naderelfeel03@gmail.com>
2025-03-30 17:58:09 +0200

    simple faceBook desktop app in java with GUI

---
+++ facebook_desktop_app_in_JAVA/viewUserFunction.java
@@ -0,0 +1,13 @@
+package com.example.demo8;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class viewUserFunction extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+    }
+}
