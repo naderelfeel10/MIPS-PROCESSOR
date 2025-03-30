c03ccd79f7d9117cae42fbd60a5c83e83b1ab396
naderelfeel10 <naderelfeel03@gmail.com>
2025-03-30 18:08:59 +0200

    MIPS Processor Design Project using VHDL

---
+++ MIPS_PROCESSOR/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "C:/Users/ELFEEL/Xilinx/MIPS_PROCESSOR/testbench_isim_beh.exe" -prj "C:/Users/ELFEEL/Xilinx/MIPS_PROCESSOR/testbench_beh.prj" "work.testbench" 
